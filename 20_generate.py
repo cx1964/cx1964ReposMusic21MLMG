2fafd4ee534e1dfa22130f5310f658617729b3a6
cx1964 <cx1964@mail.com>
2019-10-02 21:32:08 +0200

    20_generate.py fout:run  na  train inventions

--- 20_generate.py
+++ 20_generate.py
@@ -3,6 +3,7 @@
 import pickle
 import numpy
 from music21 import instrument, note, stream, chord
+
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -52,12 +53,48 @@ def prepare_sequences(notes, pitchnames, n_vocab):
 
 def create_network(network_input, n_vocab):
     """ create the structure of the neural network """
+
+    # Dit statement genereert warning
+    # WARNING:tensorflow:From /home/claude/anaconda3/lib/python3.7/site-packages/keras/backend/tensorflow_backend.py:74:
+    # The name tf.get_default_graph is deprecated. Please use tf.compat.v1.get_default_graph instead
     model = Sequential()
+    
+    # Dit statement genereert warning:
+    # WARNING:tensorflow:From /home/claude/anaconda3/lib/python3.7/site-packages/keras/backend/tensorflow_backend.py:517: The name tf.placeholder is deprecated. Please use tf.compat.v1.placeholder instead.
+    #
+    # WARNING:tensorflow:From /home/claude/anaconda3/lib/python3.7/site-packages/keras/backend/tensorflow_backend.py:4138: The name tf.random_uniform is deprecated. Please use tf.random.uniform instead.
+    #
+    # OMP: Info #212: KMP_AFFINITY: decoding x2APIC ids.
+    # OMP: Info #210: KMP_AFFINITY: Affinity capable, using global cpuid leaf 11 info
+    # OMP: Info #154: KMP_AFFINITY: Initial OS proc set respected: 0-7
+    # OMP: Info #156: KMP_AFFINITY: 8 available OS procs
+    # OMP: Info #157: KMP_AFFINITY: Uniform topology
+    # OMP: Info #179: KMP_AFFINITY: 1 packages x 4 cores/pkg x 2 threads/core (4 total cores)
+    # OMP: Info #214: KMP_AFFINITY: OS proc to physical thread map:
+    # OMP: Info #171: KMP_AFFINITY: OS proc 0 maps to package 0 core 0 thread 0 
+    # OMP: Info #171: KMP_AFFINITY: OS proc 4 maps to package 0 core 0 thread 1 
+    # OMP: Info #171: KMP_AFFINITY: OS proc 1 maps to package 0 core 1 thread 0 
+    # OMP: Info #171: KMP_AFFINITY: OS proc 5 maps to package 0 core 1 thread 1 
+    # OMP: Info #171: KMP_AFFINITY: OS proc 2 maps to package 0 core 2 thread 0 
+    # OMP: Info #171: KMP_AFFINITY: OS proc 6 maps to package 0 core 2 thread 1 
+    # OMP: Info #171: KMP_AFFINITY: OS proc 3 maps to package 0 core 3 thread 0 
+    # OMP: Info #171: KMP_AFFINITY: OS proc 7 maps to package 0 core 3 thread 1 
+    # OMP: Info #250: KMP_AFFINITY: pid 18881 tid 18881 thread 0 bound to OS proc set 0
+    # OMP: Info #250: KMP_AFFINITY: pid 18881 tid 18887 thread 1 bound to OS proc set 1
+    # OMP: Info #250: KMP_AFFINITY: pid 18881 tid 18888 thread 2 bound to OS proc set 2
+    # OMP: Info #250: KMP_AFFINITY: pid 18881 tid 18889 thread 3 bound to OS proc set 3
+
+    # ### ToDo ###
+    # Nog zoeken naar command binnen deze functie in onderstaande code die ValueError veroorzaakt
+    # Fout melding
+    # ValueError: Dimension 1 in both shapes must be equal, but are 58 and 359.
+    # Shapes are [256,58] and [256,359]. for 'Assign_11' (op: 'Assign') with input shapes: [256,58], [256,359].
     model.add(LSTM(
         512,
         input_shape=(network_input.shape[1], network_input.shape[2]),
         return_sequences=True
     ))
+    
     model.add(Dropout(0.3))
     model.add(LSTM(512, return_sequences=True))
     model.add(Dropout(0.3))
@@ -70,7 +107,6 @@ def create_network(network_input, n_vocab):
 
     # Load the weights to each node
     model.load_weights(homeDir+'weights.hdf5')
-
     return model
 
 def generate_notes(model, network_input, pitchnames, n_vocab):
