b73f9d86a055fd47945eb48cc9f7c036571bee9a
cx1964 <cx1964@mail.com>
2019-10-13 17:14:40 +0200

    10_train.py prepare_sequences() com. mbt functie

--- 10_train.py
+++ 10_train.py
@@ -55,12 +55,13 @@ def get_notes():
 
 def prepare_sequences(notes, n_vocab):
     """ Prepare the sequences used by the Neural Network """
-    sequence_length = 100
+    sequence_length = 100 # voorspel de volgende noot obv 100 voorgaande noten
 
-    # get all pitch names
+    # get all unique pitch names
     pitchnames = sorted(set(item for item in notes))
 
-     # create a dictionary to map pitches to integers
+    # create a dictionary to map pitches to integers
+    #        for number, note in enumerate(pitchnames) genereert een reeks met elementen inde vorm <rangnummer>, <pitchname>
     note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
 
     network_input = []
@@ -70,19 +71,26 @@ def prepare_sequences(notes, n_vocab):
     for i in range(0, len(notes) - sequence_length, 1):
         sequence_in = notes[i:i + sequence_length]
         sequence_out = notes[i + sequence_length]
+
+        # hier worden list met notes mapped naar een list met integers mbv gebruikt van de note_to_int dict
+        # mapping is nodig omdat neural netwerk met integers werkt om de gewichten uit te kunnen rekenen.
         network_input.append([note_to_int[char] for char in sequence_in])
+        
+        # hier worden list met notes mapped naar een list met integers mbv gebruikt van de note_to_int dict
+        # mapping is nodig omdat neural netwerk met integers werkt om de gewichten uit te kunnen rekenen.
         network_output.append(note_to_int[sequence_out])
 
     n_patterns = len(network_input)
 
     # reshape the input into a format compatible with LSTM layers
     network_input = numpy.reshape(network_input, (n_patterns, sequence_length, 1))
+    
     # normalize input
     network_input = network_input / float(n_vocab)
 
     network_output = np_utils.to_categorical(network_output)
 
-    return (network_input, network_output)
+    return (network_input, network_output) # return input en output list met mapped notes
 
 def create_network(network_input, n_vocab):
     """ create the structure of the neural network """
