491d35bcd54ba7ebd079238f48aadca9916fd0f0
cx1964 <cx1964@mail.com>
2019-11-25 21:31:08 +0100

    initial version script

---
+++ see_specific_versions_of_source_file.sh
@@ -0,0 +1,6 @@
+# See git version of 10_train.py of 4 commits ago
+git show HEAD~4:./10_train.py
+
+# Show version 80e2348b550fbf92aa82d03b161f6584f23fe609 of 10_train.py
+# See github website for version hashes
+git show 80e2348b550fbf92aa82d03b161f6584f23fe609:./10_train.py
